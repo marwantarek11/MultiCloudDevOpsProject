3c066631a0c88efd592457364698cf6892588992
MarwanTarek <167176241+marwantarek11@users.noreply.github.com>
2024-06-13 06:31:05 +0300

    Add Terraform

---
+++ Terraform/modules/VPC/variables.tf
@@ -0,0 +1,12 @@
+variable "vpc_cidr" {
+  type = string
+}
+variable "vpc_name" {
+  type = string
+}
+variable "public_subnet_cidr" {
+  type = string
+}
+variable "public_subnet_az" {
+  type = string
+}
